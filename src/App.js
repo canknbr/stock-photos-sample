40ea8163ed4bcfe939959f4950260557cc567b69
Can Kanbur <cankanbur1@gmail.com>
2022-06-06 21:18:35 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,40 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import Photo from './Photo';
+import React, { useState, useEffect, useRef } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
+import Photo from './Photo';
+const clientID = `?client_id=KiBehg2aSR_vbRU9qN22AntTKtGQQmNqrS3wdvx7I5Y`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
-const App = () => {
-  const [photos, setPhotos] = useState([]);
-  const [search, setSearch] = useState('');
+
+function App() {
   const [loading, setLoading] = useState(false);
-  const getFetch = async () => {
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const mounted = useRef(false);
+  const [newImages, setNewImages] = useState(false);
+  const fetchImages = async () => {
     setLoading(true);
     let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
     try {
-      url = `${mainUrl}?client_id=KiBehg2aSR_vbRU9qN22AntTKtGQQmNqrS3wdvx7I5Y`;
       const response = await fetch(url);
       const data = await response.json();
-
-      setPhotos(data);
+      setPhotos(oldPhotos => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
       setLoading(false);
     } catch (error) {
+      setNewImages(false);
+
       setLoading(false);
-      console.log(error);
     }
   };
   useEffect(() => {
-    getFetch();
-  }, [search]);
+    fetchImages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (loading) return;
+    setPage(oldPage => oldPage + 1);
+  }, [newImages]);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+    return () => window.removeEventListener('scroll', event);
+  }, []);
+
   const handleSubmit = e => {
     e.preventDefault();
+    if (!query) return;
+    if (page === 1) {
+      fetchImages();
+    }
+    setPage(1);
   };
   return (
     <main>
       <h2 className="sample-title">Stock Photos Sample</h2>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="Search..." />
+          <input
+            type="text"
+            placeholder="search"
+            value={query}
+            onChange={e => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
@@ -42,14 +95,14 @@ const App = () => {
       </section>
       <section className="photos">
         <div className="photos-center">
-          {photos.map((photo, index) => {
-            return <Photo key={index} {...photo} />;
+          {photos.map((image, index) => {
+            return <Photo key={index} {...image} />;
           })}
         </div>
         {loading && <h2 className="loading">Loading...</h2>}
       </section>
     </main>
   );
-};
+}
 
 export default App;
